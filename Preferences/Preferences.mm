098302627a20768985e9de00ffab56bb1b33aab6
Grant Paul <chpwn@chpwn.com>
2011-12-15 22:35:04 -0800

    Update preferences.

--- Preferences/Preferences.mm
+++ Preferences/Preferences.mm
@@ -19,5 +19,13 @@
     return _specifiers;
 }
 
+- (void)moreInfoPressed:(id)specifier {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://chpwn.com/apps/spire"]];
+}
+
+- (void)donatePressed:(id)specifier {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=2AZTMSJJ9YPU2&lc=US&item_name=Spire%20Donation&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted"]];
+}
+
 @end
 
