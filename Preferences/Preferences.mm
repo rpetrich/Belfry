8f1412c81845a384218b0443cff823c2a85ec43b
Grant Paul <chpwn@chpwn.com>
2011-12-18 22:36:17 -0800

    Update preferences URLs. None of these are live yet.

--- Preferences/Preferences.mm
+++ Preferences/Preferences.mm
@@ -20,11 +20,11 @@
 }
 
 - (void)moreInfoPressed:(id)specifier {
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://chpwn.com/apps/spire"]];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://chpwn.com/apps/spire/help"]];
 }
 
 - (void)donatePressed:(id)specifier {
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=2AZTMSJJ9YPU2&lc=US&item_name=Spire%20Donation&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted"]];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://chpwn.com/apps/spire/donate"]];
 }
 
 @end
