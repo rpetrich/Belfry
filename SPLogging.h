3426d00be71b77b69b29029ad69ce1da2e8d6c5c
Grant Paul <chpwn@chpwn.com>
2011-12-22 17:35:30 -0800

    Flush log output.

--- SPLogging.h
+++ SPLogging.h
@@ -1,3 +1,3 @@
 
-#define SPLog(...) printf("%s\n", [[NSString stringWithFormat:__VA_ARGS__] UTF8String])
+#define SPLog(...) ((^{printf("%s\n", [[NSString stringWithFormat:__VA_ARGS__] UTF8String]);fflush(stdout);})())
 
